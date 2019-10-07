ffbc77e208679699c3ba6626fbe79ddda0742dd2
Arpit Garg <41018882+garpit421@users.noreply.github.com>
2019-10-07 15:17:36 +0530

    Update next_greater_element.py
    adding comment to under stand and all library and main function

--- data_structures/stacks/next_greater_element.py
+++ data_structures/stacks/next_greater_element.py
@@ -1,17 +1,32 @@
-# Function to print element and NGE pair for all elements of list
-def printNGE(arr):
-
-    for i in range(0, len(arr), 1):
-
-        next = -1
-        for j in range(i + 1, len(arr), 1):
-            if arr[i] < arr[j]:
-                next = arr[j]
-                break
-
-        print(str(arr[i]) + " -- " + str(next))
-
-
-# Driver program to test above function
-arr = [11, 13, 21, 3]
-printNGE(arr)
+#include<iostream> 
+using namespace std; 
+  
+/* prints element and NGE pair  
+for all elements of arr[] of size n */
+void printNGE(int arr[], int n) 
+{ 
+    int next, i, j; 
+    for (i = 0; i < n; i++) 
+    { 
+        next = -1; 
+        for (j = i + 1; j < n; j++) 
+        { 
+            if (arr[i] < arr[j]) 
+            { 
+                next = arr[j]; 
+                break; 
+            } 
+        } 
+        cout << arr[i] << " -- " 
+             << next << endl; 
+    } 
+} 
+  
+// Driver Code 
+int main() 
+{ 
+    int arr[] = {11,22, 13, 21, 3}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    printNGE(arr, n); 
+    return 0; 
+} 
